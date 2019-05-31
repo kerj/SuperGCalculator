bd24dc32125b830f36c1901825cd792cbf684604
jk <kerntzj@gmail.com>
2019-05-31 13:02:25 -0700

    add test for mars planet age

--- test/galacticAge.test.js
+++ test/galacticAge.test.js
@@ -1,5 +1,6 @@
 import { ageInMillie } from '../src/galacticAge.js';
 import { millieToYears } from '../src/galacticAge.js';
+import { planetAge } from '../src/galacticAge.js';
 
 test('returns the number of millieseconds from since a date object', () => {
     let testVar = ageInMillie('1992-5-1');
@@ -10,6 +11,11 @@ test('returns the number of millieseconds from since a date object', () => {
 test('return number of years from millieseconds', () => {
   let testVar = millieToYears('1992-5-1');
   expect(testVar).toBe(27);
+})
+
+test('return age in mars years', () => {
+  let testVar = planetAge("Mars", '1992-5-1');
+  expect(testVar).toBe(14);
 })
 // test('adds 5 + 5 to equal 10', () => {
 //     expect(add(5, 5)).toBe(10);
