00989d5f65a219c3ae7ae280fbae32f73a7f105e
jk <kerntzj@gmail.com>
2019-05-31 16:51:47 -0700

    refactored into class conventions

--- src/galacticAge.js
+++ src/galacticAge.js
@@ -1,71 +1,72 @@
 export class PersonProfile {
-
   constructor(planet, bday){
     this.planet = planet;
     this.bday = bday;
   }
   get age() {
-    return planetAge(planet, bday);
-  }
+    return this.planetAge();
+  };
 
   get lifeExpectancy() {
-    return lifeExpectancyChecker(bday);
-  }
+    return this.lifeExpectancyChecker();
+  };
 
-  ageInMillie(bday) {
+  get ageInMillie() {
     const millisecondsInYear = 31536000000;
 
     let todayDate = new Date();
     let todayInMillie = todayDate.getTime();
 
-    let birthdayDateObject = new Date(bday);
+    let birthdayDateObject = new Date(this.bday);
     let millesecondsSinceJSstart = birthdayDateObject.getTime();
 
     bday = Math.abs(todayInMillie - millesecondsSinceJSstart);
     let yearsAge = Math.round(bday/millisecondsInYear);
     bday = yearsAge*millisecondsInYear;
-    return bday;
+    return this.bday;
   };
 
-  millieToYears(bday) {
+  get millieToYears() {
     const millisecondsInYear = 31536000000;
 
-    let ageInMilleseconds = this.ageInMillie(bday);
+    let ageInMilleseconds = this.ageInMillie();
     let yearsAge = Math.round(ageInMilleseconds/millisecondsInYear);
-    return yearsAge;
-  }
+    return this.yearsAge;
+  };
 
-  planetAge(planet, bday) {
+  get planetAge() {
     switch(this.planet) {
       case "Earth":
-      let earthAge = this.millieToYears(bday);
-      return earthAge;
+      let earthAge = this.millieToYears();
+      return this.earthAge;
       case "Mercury":
-      let mercuryAge = Math.round((this.millieToYears(bday)/.24));
-      return mercuryAge;
+      let mercuryAge = Math.round((this.millieToYears()/.24));
+      return this.mercuryAge;
       case "Venus":
-      let venusAge = Math.round((this.millieToYears(bday)/.62));
-      return venusAge;
+      let venusAge = Math.round((this.millieToYears()/.62));
+      return this.venusAge;
       case "Mars":
-      let marsAge = Math.round((this.millieToYears(bday)/1.88));
-      return marsAge;
+      let marsAge = Math.round((this.millieToYears()/1.88));
+      return this.marsAge;
       case "Jupiter":
-      let jupiterAge = Math.round((this.millieToYears(bday)/11.86));
-      return jupiterAge;
+      let jupiterAge = Math.round((this.millieToYears()/11.86));
+      return this.jupiterAge;
     }
-  }
+  };
 
-  lifeExpectancyChecker(bday) {
+  get lifeExpectancyChecker() {
     const lifeExpectancy = 78;
-    let ageVsExpectancy = (lifeExpectancy-this.millieToYears(bday));
+    let ageVsExpectancy = (lifeExpectancy-this.millieToYears());
     if (ageVsExpectancy < 0) {
       let remainingYears = Math.abs(ageVsExpectancy);
-      return remainingYears;
+      return this.remainingYears;
     }else if (ageVsExpectancy > 0) {
-      return ageVsExpectancy;
+      return this.ageVsExpectancy;
     }else {
       let exactAgeExpectancy = "You have reached the average age!!";
-      return exactAgeExpectancy;
+      return this.exactAgeExpectancy;
     }
-  }
-}
+  };
+};
+
+module.exports = PersonProfile;
