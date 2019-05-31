df29fe69caf3c9e96c69676e503b0259e71bee6e
jk <kerntzj@gmail.com>
2019-05-31 15:41:06 -0700

    refactor to use classes and methods

--- src/galacticAge.js
+++ src/galacticAge.js
@@ -1,51 +1,71 @@
-export function ageInMillie(bday) {
-  let todayDate = new Date();
-  let todayInMillie = todayDate.getTime();
+export class PersonProfile {
 
-  let birthdayDateObject = new Date(bday);
-  let millesecondsSinceJSstart = birthdayDateObject.getTime();
-
-  bday = Math.abs(todayInMillie - millesecondsSinceJSstart);
-  let yearsAge = Math.round(bday/31536000000);
-  bday = yearsAge*31536000000;
-  return bday;
-};
-
-export function millieToYears(bday) {
-  let ageInMilleseconds = ageInMillie(bday);
-  let yearsAge = Math.round(ageInMilleseconds/31536000000);
-  return yearsAge;
-}
-
-export function planetAge(planet, bday) {
-  switch(planet) {
-    case "Earth":
-      let earthAge = millieToYears(bday);
-      return earthAge;
-    case "Mercury":
-      let mercuryAge = Math.round((millieToYears(bday)/.24));
-      return mercuryAge;
-    case "Venus":
-      let venusAge = Math.round((millieToYears(bday)/.62));
-      return venusAge;
-    case "Mars":
-      let marsAge = Math.round((millieToYears(bday)/1.88));
-      return marsAge;
-    case "Jupiter":
-      let jupiterAge = Math.round((millieToYears(bday)/11.86));
-      return jupiterAge;
+  constructor(planet, bday){
+    this.planet = planet;
+    this.bday = bday;
   }
-}
+  get age() {
+    return planetAge(planet, bday);
+  }
 
-export function lifeExpectancyChecker(bday) {
-  let ageVsExpectancy = (78-millieToYears(bday));
-  if (ageVsExpectancy < 0) {
-    let remainingYears = Math.abs(ageVsExpectancy);
-    return remainingYears;
-  }else if (ageVsExpectancy > 0) {
-    return ageVsExpectancy;
-  }else {
-    let exactAgeExpectancy = "You have reached the average age!!";
-    return exactAgeExpectancy;
+  get lifeExpectancy() {
+    return lifeExpectancyChecker(bday);
+  }
+
+  ageInMillie(bday) {
+    const millisecondsInYear = 31536000000;
+
+    let todayDate = new Date();
+    let todayInMillie = todayDate.getTime();
+
+    let birthdayDateObject = new Date(bday);
+    let millesecondsSinceJSstart = birthdayDateObject.getTime();
+
+    bday = Math.abs(todayInMillie - millesecondsSinceJSstart);
+    let yearsAge = Math.round(bday/millisecondsInYear);
+    bday = yearsAge*millisecondsInYear;
+    return bday;
+  };
+
+  millieToYears(bday) {
+    const millisecondsInYear = 31536000000;
+
+    let ageInMilleseconds = this.ageInMillie(bday);
+    let yearsAge = Math.round(ageInMilleseconds/millisecondsInYear);
+    return yearsAge;
+  }
+
+  planetAge(planet, bday) {
+    switch(this.planet) {
+      case "Earth":
+      let earthAge = this.millieToYears(bday);
+      return earthAge;
+      case "Mercury":
+      let mercuryAge = Math.round((this.millieToYears(bday)/.24));
+      return mercuryAge;
+      case "Venus":
+      let venusAge = Math.round((this.millieToYears(bday)/.62));
+      return venusAge;
+      case "Mars":
+      let marsAge = Math.round((this.millieToYears(bday)/1.88));
+      return marsAge;
+      case "Jupiter":
+      let jupiterAge = Math.round((this.millieToYears(bday)/11.86));
+      return jupiterAge;
+    }
+  }
+
+  lifeExpectancyChecker(bday) {
+    const lifeExpectancy = 78;
+    let ageVsExpectancy = (lifeExpectancy-this.millieToYears(bday));
+    if (ageVsExpectancy < 0) {
+      let remainingYears = Math.abs(ageVsExpectancy);
+      return remainingYears;
+    }else if (ageVsExpectancy > 0) {
+      return ageVsExpectancy;
+    }else {
+      let exactAgeExpectancy = "You have reached the average age!!";
+      return exactAgeExpectancy;
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
-import { ageInMillie } from '../src/galacticAge.js';
-$(document).ready(function() {
-  var showME = ageInMillie('1992-5-1');
-  $("#output").html(showME);
-});
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './styles.css';
+// import { ageInMillie } from '../src/galacticAge.js';
+// $(document).ready(function() {
+//   var showME = ageInMillie('1992-5-1');
+//   $("#output").html(showME);
+// });
